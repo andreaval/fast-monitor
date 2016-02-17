b0398515a10b0340f090780e5d9986eaea52d0f9
Andrea <andrea.vallorani@gmail.com>
2016-02-17 16:45:26 +0100

    cambiato posizione config

--- install.js
+++ install.js
@@ -1,8 +1,7 @@
 #! /usr/bin/env node
 try{
-    var o = require('child_process').execSync("cp -n config.sample.js /usr/local/etc/fast-monitor.js");
-    console.log(o);
+    require('child_process').execSync("cp -n config/config.sample.js config/default.js");
 }
 catch(e){
-    console.log('Config file exists, do not overwrite.');
+    console.log(e);
 }
